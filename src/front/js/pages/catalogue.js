af73eca197166e728c67978fec87f43e80bd2b01
lexie70 <alexandracampos658@gmail.com>
2023-04-22 00:14:16 +0000

    trabajando en el catalogo

--- src/front/js/pages/catalogue.js
+++ src/front/js/pages/catalogue.js
@@ -1,56 +1,65 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import Card from "../component/card";
 import "../../styles/home.css";
+import "../../styles/index.css";
+
 
 export const Catalogue= () => {
 	const { store, actions } = useContext(Context);
 	const [shoes,setShoes]= useState([])
+	const [category,setCategory]= useState({})
 
 	useEffect(()=>{
 		fetch(process.env.BACKEND_URL+"/api/get/shoes")
 		.then(res=> res.json())
-		.then(data=> setShoes(data))
+		.then(data=> {setShoes(data);console.log(data)})
 		.catch(error=> console.log(error))
 
-	})
+	},[])
 
 	return (
 		<>
-		{/* <div className="d-flex justify-content-center">
-			<Card />
-			<Card />
-			<Card />
-		</div>
-
-		
-		<div className="d-flex justify-content-center">
-			<Card />
-			<Card />
-			<Card />
-		</div> */}
-		<div onClick={()=>console.log(shoes)}>hello</div>
-		<ul>
+	<section className="breadcrumb">
+  		<a href="#">Categoría</a> 
+  		<a href="#">Subcategoría</a> 
+	</section>
+	<div onClick={()=>console.log(shoes)}></div>
+		<section className="card">
 		{shoes.map((item,index)=>{
-				
-		return	<ShoeCard name={item.name} imageUrl={item.photo}key={index} />
-	
+			return	<ShoeCard name={item.name} imageUrl={item.photo} category={item.category.name} sizeFrom={item.size_from}
+		 	sizeTo={item.size_to} key={index} />
 		})}
+		</section>	
+				
+	
 		
-	</ul>	
 	</>
 	);
+											
+  		
+		
 };
 
-function ShoeCard ({name, imageUrl}) {
+function ShoeCard ({name, imageUrl,category,sizeFrom, sizeTo}) {
 	return <>
+	
 	<div className="shoe-card">
-		<h2>{name}</h2>
+		<h2  className="bg-pink">{name}</h2>
 		<div>
 			<img src={imageUrl} alt=""/>
 		</div>
-		<button> <i className="fa-regular fa-heart"></i></button>
+		
+		<div className="next-to-heart bold ">
+			<div>
+				<span className="">Disponibles Desde<strong>{sizeFrom}</strong>  Hasta:<strong>{sizeTo}</strong></span>
+
+			</div>
+			<button className="heart"> <i className="fa-regular fa-heart"></i></button>
+		</div>
 	</div>
+
+
+	
 	
 	
 	</>
